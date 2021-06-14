0e1d6e2a92029fe391b3f9f0dc62781b460e5868
BekaSlayer <bekaslayer@gmail.com>
2021-06-14 13:47:18 +0200

    QueryEngine-001: Added simple UI.

--- QueryEngine/MainWindow.xaml.cs
+++ QueryEngine/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace QueryEngine
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataSourceTextBlock.Text = QueryTextBox.Text;
+        }
     }
 }
