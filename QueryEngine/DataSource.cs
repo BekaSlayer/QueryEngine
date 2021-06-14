3da2c336850cd5811e36dea9617952eeaeeba5d0
BekaSlayer <bekaslayer@gmail.com>
2021-06-14 13:56:49 +0200

    QueryEngine-002: Added data source to solution.

---
+++ QueryEngine/DataSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryEngine
+{
+    public sealed class DataSource
+    {
+        public List<User> Users { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+    public sealed class User
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; } 
+    }
+    
+    public sealed class Order
+    {
+        public string BuyerName { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+    }
+}
